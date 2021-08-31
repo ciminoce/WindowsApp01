a29e2cd61c3a85c0451955ce4178baa47370bb8c
ciminoce <ciminoce@yahoo.com.ar>
2021-08-31 20:05:07 -0300

    Agregar archivos de proyecto.

---
+++ WindowsApp01/FrmSuma.Designer.cs
@@ -0,0 +1,147 @@
+﻿
+namespace WindowsApp01
+{
+    partial class FrmSuma
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.CerrarButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Numero1TextBox = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Numero2TextBox = new System.Windows.Forms.TextBox();
+            this.SumarButton = new System.Windows.Forms.Button();
+            this.CancelarButton = new System.Windows.Forms.Button();
+            this.SumasListBox = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // CerrarButton
+            // 
+            this.CerrarButton.Location = new System.Drawing.Point(680, 383);
+            this.CerrarButton.Name = "CerrarButton";
+            this.CerrarButton.Size = new System.Drawing.Size(108, 55);
+            this.CerrarButton.TabIndex = 4;
+            this.CerrarButton.Text = "Cerrar";
+            this.CerrarButton.UseVisualStyleBackColor = true;
+            this.CerrarButton.Click += new System.EventHandler(this.CerrarButton_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(42, 44);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(56, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Numero 1:";
+            // 
+            // Numero1TextBox
+            // 
+            this.Numero1TextBox.Location = new System.Drawing.Point(121, 41);
+            this.Numero1TextBox.Name = "Numero1TextBox";
+            this.Numero1TextBox.Size = new System.Drawing.Size(100, 20);
+            this.Numero1TextBox.TabIndex = 0;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(45, 97);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Número 2:";
+            // 
+            // Numero2TextBox
+            // 
+            this.Numero2TextBox.Location = new System.Drawing.Point(121, 94);
+            this.Numero2TextBox.Name = "Numero2TextBox";
+            this.Numero2TextBox.Size = new System.Drawing.Size(100, 20);
+            this.Numero2TextBox.TabIndex = 1;
+            // 
+            // SumarButton
+            // 
+            this.SumarButton.Location = new System.Drawing.Point(45, 147);
+            this.SumarButton.Name = "SumarButton";
+            this.SumarButton.Size = new System.Drawing.Size(92, 40);
+            this.SumarButton.TabIndex = 2;
+            this.SumarButton.Text = "Sumar";
+            this.SumarButton.UseVisualStyleBackColor = true;
+            this.SumarButton.Click += new System.EventHandler(this.SumarButton_Click);
+            // 
+            // CancelarButton
+            // 
+            this.CancelarButton.Location = new System.Drawing.Point(177, 147);
+            this.CancelarButton.Name = "CancelarButton";
+            this.CancelarButton.Size = new System.Drawing.Size(92, 40);
+            this.CancelarButton.TabIndex = 3;
+            this.CancelarButton.Text = "Cancelar";
+            this.CancelarButton.UseVisualStyleBackColor = true;
+            this.CancelarButton.Click += new System.EventHandler(this.CancelarButton_Click);
+            // 
+            // SumasListBox
+            // 
+            this.SumasListBox.FormattingEnabled = true;
+            this.SumasListBox.Location = new System.Drawing.Point(48, 209);
+            this.SumasListBox.Name = "SumasListBox";
+            this.SumasListBox.Size = new System.Drawing.Size(221, 225);
+            this.SumasListBox.TabIndex = 5;
+            // 
+            // FrmSuma
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(224)))), ((int)(((byte)(224)))));
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ControlBox = false;
+            this.Controls.Add(this.SumasListBox);
+            this.Controls.Add(this.CancelarButton);
+            this.Controls.Add(this.SumarButton);
+            this.Controls.Add(this.Numero2TextBox);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.Numero1TextBox);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.CerrarButton);
+            this.Name = "FrmSuma";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Suma de Números";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button CerrarButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox Numero1TextBox;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox Numero2TextBox;
+        private System.Windows.Forms.Button SumarButton;
+        private System.Windows.Forms.Button CancelarButton;
+        private System.Windows.Forms.ListBox SumasListBox;
+    }
+}
+
---
+++ WindowsApp01/FrmSuma.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsApp01
+{
+    public partial class FrmSuma : Form
+    {
+        public FrmSuma()
+        {
+            InitializeComponent();
+        }
+
+        private void CerrarButton_Click(object sender, EventArgs e)
+        {
+            Application.Exit();//Cierra los forms y sale de la aplicación
+        }
+
+        private void SumarButton_Click(object sender, EventArgs e)
+        {
+            int numero1;
+            int numero2;
+            int suma;
+            numero1 = int.Parse(Numero1TextBox.Text);//Tomo el contenido de la prop Text del cuadro de texto
+            numero2 = int.Parse(Numero2TextBox.Text);
+            suma = numero1 + numero2;
+            MessageBox.Show($"La suma es {suma}","Resultado",MessageBoxButtons.OK,MessageBoxIcon.Information);
+
+            SumasListBox.Items.Add(suma);
+
+            InicializarControles();
+        }
+
+        private void CancelarButton_Click(object sender, EventArgs e)
+        {
+            InicializarControles();
+        }
+
+        private void InicializarControles()
+        {
+            Numero1TextBox.Clear();
+            Numero2TextBox.Clear();
+            Numero1TextBox.Focus();
+        }
+    }
+}
